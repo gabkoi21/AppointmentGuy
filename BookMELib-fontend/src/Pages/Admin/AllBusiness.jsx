8b8dcdb552016d423242b102bbd43688c9fc474f
gabkoi21 <12345678+username@users.noreply.github.com>
2025-04-09 01:05:51 +0000

    Add the Admin board and the business

--- BookMELib-fontend/src/Pages/Admin/AllBusiness.jsx
+++ BookMELib-fontend/src/Pages/Admin/AllBusiness.jsx
@@ -1,30 +1,60 @@
-import { from } from "list";
+import React, { useState } from "react";
 import GlobalSearchBar from "../../components/common/globalSearchBar";
 import GlobalButton from "../../components/common/globalButton";
-import GlobalFilter from "../../components/common/globalFilter";
 import DataTable from "../../components/common/DataTable";
 import { columns } from "../../components/Admin/Dashboard/BusinessColumns";
 import BusinessRow from "../../components/Admin/Dashboard/BusinessRow";
 import { businessData } from "../../data/BusinesseData";
-import SearchBusiness from "../../components/Admin/Dashboard/SearchBusinessStatus";
-import { useState } from "react";
+import ActiveStatusNavigation from "../../components/common/StatusNavigation";
+
+import Icon from "@mdi/react";
+import { mdiPlus } from "@mdi/js";
 
 const AllBusinessContainer = () => (
   <div className="flex">
     <aside className="md:w-[20%] lg:w-[23%] h-screen bg-gray-100 dark:bg-gray-800" />
-    <main className="md:w-[98%] w-full mx-3 px-3 mt-20">
+    <main className="md:w-[98%] w-full mx-3 px-3 mt-28">
       <AllBusiness />
     </main>
   </div>
 );
 
 const AllBusiness = () => {
+  const [activeTab, setActiveTab] = useState("All Business");
   return (
-    <div>
-      <p>This is all businees Page</p>
-      {/* <SearchBusiness /> */}
-      <GlobalFilter />
+    <>
+      <div className="flex  gap-4 justify-between items-center mb-4">
+        <div>
+          <ActiveStatusNavigation
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        </div>
+        <div className="flex gap-4">
+          <GlobalButton text=" + Add Business" />
+        </div>
+      </div>
       <GlobalSearchBar />
+      <BusinessTable activeTab={activeTab} />
+    </>
+  );
+};
+
+const BusinessTable = ({ activeTab }) => {
+  return (
+    <div className="mt-5">
+      {activeTab === "All Business" && (
+        <DataTable
+          columns={columns}
+          data={businessData}
+          renderRow={(item, index) => (
+            <BusinessRow key={index} business={item} />
+          )}
+        />
+      )}
+      {activeTab === "Pending Approved" && <p>Pending approval businesses</p>}
+      {activeTab === "Active" && <p>Active businesses</p>}
+      {activeTab === "Suspend" && <p>Suspended businesses</p>}
     </div>
   );
 };
--- BookMELib-fontend/src/components/Admin/Dashboard/SearchBusinessStatus.jsx
+++ BookMELib-fontend/src/components/Admin/Dashboard/SearchBusinessStatus.jsx
@@ -6,69 +6,46 @@ import DataTable from "../../../components/common/DataTable";
 import { columns } from "./BusinessColumns";
 import BusinessRow from "./BusinessRow";
 import { businessData } from "../../../data/BusinesseData";
+import ActiveStatusNavigation from "../../common/StatusNavigation";
 
 const SearchBusiness = () => {
   const [activeTab, setActiveTab] = useState("All Business");
+
   return (
     <>
-      <div className="flex justify-between items-center mt-10">
-        {/* This is for the Active Status navigation */}
-
-        <div className="flex items-center bg-gray-100 text-black rounded-lg py-1 px-2 gap-2">
-          <ActiveStatusNavigation />
+      <div className="flex items-center justify-between mt-5">
+        <div>
+          <ActiveStatusNavigation
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
         </div>
-        {/* This is for the Global Filter and Add Business button */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
           <GlobalFilter />
           <GlobalButton text="Add Business" />
         </div>
       </div>
-      {/* This is for the Global Filter and Add Business button */}
-      <div className="mt-5 relative">
-        <GlobalSearchBar />
-      </div>
-      {/* This is for the Data Table */}
-      <div>
-        <DisplayBusiness activeTab={activeTab} />
-      </div>
+      <GlobalSearchBar placeholder="Search Business" />
+      <BusinessTable activeTab={activeTab} />
     </>
   );
 };
 
-const DisplayBusiness = ({ activeTab }) => {
+const BusinessTable = ({ activeTab }) => {
   return (
-    <>
-      <div className="mt-5">
-        {activeTab === "All Business" && (
-          <DataTable
-            columns={columns}
-            data={businessData}
-            renderRow={(item, index) => (
-              <BusinessRow key={index} business={item} />
-            )}
-          />
-        )}
-      </div>
-    </>
-  );
-};
-
-const ActiveStatusNavigation = () => {
-  const tabs = ["All Business", "Pending Approved", "Active", "Suspend"];
-  return (
-    <div className="flex items-center bg-gray-100 text-black rounded-lg py-1 px-2 gap-2">
-      <button className="px-4 py-1 rounded text-black font-medium transition duration-200 bg-white">
-        All Business
-      </button>
-      <button className="px-4 py-1 rounded text-black font-medium transition duration-200">
-        Pending Approved
-      </button>
-      <button className="px-4 py-1 rounded text-black font-medium transition duration-200">
-        Active
-      </button>
-      <button className="px-4 py-1 rounded text-black font-medium transition duration-200">
-        Suspend
-      </button>
+    <div className="mt-5">
+      {activeTab === "All Business" && (
+        <DataTable
+          columns={columns}
+          data={businessData}
+          renderRow={(item, index) => (
+            <BusinessRow key={index} business={item} />
+          )}
+        />
+      )}
+      {activeTab === "Pending Approved" && <p>Pending approval businesses</p>}
+      {activeTab === "Active" && <p>Active businesses</p>}
+      {activeTab === "Suspend" && <p>Suspended businesses</p>}
     </div>
   );
 };
