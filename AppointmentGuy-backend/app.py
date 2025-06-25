ec6f562ba44f53c8f1622b431aca8e2f69d728e9
gabkoi21 <gabakoi21@gmail.com>
2025-06-25 01:05:01 +0000

    fix: enable CORS for frontend access

--- AppointmentGuy-backend/app.py
+++ AppointmentGuy-backend/app.py
@@ -38,7 +38,8 @@ def create_app(db_url=None):
     app = Flask(__name__)
 
     # ✅ Enable CORS
-    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    # CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    CORS(app, supports_credentials=True, origins="[https://appointmentguy-frontend.onrender.com]")
 
     # ✅ API Metadata
     app.config["API_TITLE"] = "BookMeLib REST API"
